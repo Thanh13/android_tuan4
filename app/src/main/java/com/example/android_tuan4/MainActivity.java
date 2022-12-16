16afc88acb0935b47712c2b26eba7f4dcb9b2505
Admin <phamlethanh2021@gmail.com>
2022-12-16 11:11:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android_tuan4/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android_tuan4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.android_tuan4.ui.main.MainFragment;
+import com.example.android_tuan4.ui.main.SendingInteface;
+
+public class MainActivity extends AppCompatActivity implements SendingInteface {
+    private Fragment fragmentCenter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+//        fragmentCenter = findViewById(R.id.fragment);
+        Intent intent = getIntent();
+        String giaTien = intent.getStringExtra("giatien");
+
+        Bundle bundle = new Bundle();
+        bundle.putString("giatien", giaTien);
+    }
+
+    @Override
+    public void sendData(String data) {
+//        TextView textView = setText(data);
+    }
+}
---
+++ app/src/main/java/com/example/android_tuan4/main/SendingInteface.java
@@ -0,0 +1,5 @@
+package com.example.android_tuan4.main;
+
+public interface SendingInteface {
+    void sendData(String data);
+}
